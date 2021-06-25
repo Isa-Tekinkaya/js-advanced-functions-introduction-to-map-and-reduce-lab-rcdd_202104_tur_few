aced63c3cddaa96a88ce938a4125f72a0239f943
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-06-25 12:10:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function mapToNegativize(array){
+  return array.map(array => array * -1)
+}
+function mapToNoChange(array) {
+  return array.map(array => array)
+  
+}
+function mapToDouble(array){
+  return array.map(array => array *2)
+} 
+function mapToSquare(array){
+  return array.map(array => array * array)
+}
+
+  function reduceToTotal(sourceArray, startingPoint=0){
+    return sourceArray.reduce((acc, cur) => {
+      return  cur+  acc     
+     
+  },startingPoint )
+    
+}
+  
+  
+  function reduceToAllTrue(array){
+    return array.reduce((sourceArray) => {sourceArray
+   })
+    
+  }
